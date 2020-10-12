8127926ed84a96518ec04661a4fe8e5f9b333c06
yeonhodev <yeonho.dev@gmail.com>
2020-10-12 16:46:56 -0400

    listen and response by voice

--- listen.py
+++ listen.py
@@ -1,4 +1,7 @@
 import speech_recognition
+import pyttsx3
+
+engine = pyttsx3.init()
 
 recognizer = speech_recognition.Recognizer()
 with speech_recognition.Microphone() as source:
@@ -7,3 +10,24 @@ with speech_recognition.Microphone() as source:
 
 print("You said:")
 print(recognizer.recognize_google(audio))
+
+words = recognizer.recognize_google(audio)
+
+if "hello" in words:
+    print("Hello to you too!")
+    engine.say("Hello to you too!")
+    engine.runAndWait()
+elif "how are you" in words:
+    print("I am well, thanks!")
+    engine.say("I am well, thanks!")
+    engine.runAndWait()
+elif "prettiest" in words:
+    print("It's definitely Carol!")
+    engine.say("It's definitely Carol!")
+    engine.runAndWait()
+elif "goodbye" in words:
+    print("Goodbye to you too!")
+    engine.say("Goodbye to you too!")
+    engine.runAndWait()
+else:
+    print("Huh?")
